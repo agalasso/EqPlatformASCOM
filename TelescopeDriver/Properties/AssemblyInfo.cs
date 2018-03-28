262b343688afc0203b4093a81de6db0069aa4d3e
Andy Galasso <andy.galasso@gmail.com>
2018-03-28 01:26:44 -0400

    initial commit

---
+++ TelescopeDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.EqPlatformAdapter.Telescope")]
+[assembly: AssemblyDescription("ASCOM Telescope driver for Equatorial Platform Adapter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("adgsoftware.com")]
+[assembly: AssemblyProduct("ASCOM Telescope driver for EqPlatformAdapter")]
+[assembly: AssemblyCopyright("Copyright © 2018 Andy Galasso and Thomas Beck")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f082bf1c-9b88-4aa2-b094-0e8ce0d94254")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.2.*")]
+[assembly: AssemblyFileVersion("6.2.0.0")]
